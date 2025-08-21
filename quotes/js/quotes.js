ba7bcd1527aef96d70f66113f62c49f9ebc60209
greenish06 <greenishvekariya1080@gmail.com>
2025-08-21 17:34:16 +0530

    slider

---
+++ quotes/js/quotes.js
@@ -0,0 +1,359 @@
+const Data = [
+
+    {quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",author: "Nelson Mandela",imgurl: "https://imgs.search.brave.com/QzCFbjwOydjkU8044s_ZmKE5KM1tSl6lTVfDnd77yV4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA4LzIwLzA3Lzg5/LzM2MF9GXzgyMDA3/ODk1MV9uczZkbFN2/VEQ4eGZBSlFhZWd0/U3JUYjk5WWp0dGV4/MS5qcGc",birthPlace: "Mvezo, South Africa"},
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+
+        author: "Walt Disney",
+
+        imgurl: "https://imgs.search.brave.com/YyYYWyvsMCm280zFnGbpo2UFS5urCooLtFjqMLpe7C4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFNTUJQTFU0U0wu/anBn",
+
+        birthPlace: "Chicago, Illinois, USA"
+
+    },
+
+    {
+
+        quote: "Your time is limited, so don't waste it living someone else's life.",
+
+        author: "Steve Jobs",
+
+        imgurl: "https://imgs.search.brave.com/tU_V-KbjJDODsFtZvUPHd13ztrNwj6TrA5rtCxZ07w4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlpHTXhOalZr/WW1RdE5UTTBOUzAw/TXpCa0xXSTVPR0l0/WW1JM05Ea3lObUZs/WVdZNVhrRXlYa0Zx/Y0djQC5qcGc",
+
+        birthPlace: "San Francisco, California, USA"
+
+    },
+
+    {
+
+        quote: "If life were predictable it would cease to be life, and be without flavor.",
+
+        author: "Eleanor Roosevelt",
+
+        imgurl: "https://imgs.search.brave.com/hPw6z92Bzvx3RfcUuj1BidtsoebUibMrhJjex0EmEaY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTF5cmNnMVVWRUwu/anBn",
+
+        birthPlace: "New York City, New York, USA"
+
+    },
+
+    {
+
+        quote: "If you look at what you have in life, you'll always have more.",
+
+        author: "Oprah Winfrey",
+
+        imgurl: "https://imgs.search.brave.com/_aWtSeLZnYdPB5MKLs60_UP5zPOnRxvpU89tAry8ewo/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk5EZG1OalF3/TWpNdE5EWmtNaTAw/WW1Sa0xUZ3haall0/T0RFMFlXTmlNMlJs/T1RJM1hrRXlYa0Zx/Y0djQC5qcGc",
+
+        birthPlace: "Kosciusko, Mississippi, USA"
+
+    },
+
+    {
+
+        quote: "Life is what happens when you're busy making other plans.",
+
+        author: "John Lennon",
+
+        imgurl: "https://imgs.search.brave.com/2KGVdKkHCpGH2VJt1rVBRk1Em5b_7Zmrk-9nwX937Dg/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vNTQvMTc4MzU0/LTA1MC03RTIzOTY0/OC9Kb2huLUxlbm5v/bi1wdWJsaWNpdHkt/SGVscC1SaWNoYXJk/LUxlc3Rlci5qcGc_/dz0zMDA",
+
+        birthPlace: "Liverpool, England"
+
+    },
+
+    {
+
+        quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+
+        author: "Mother Teresa",
+
+        imgurl: "https://imgs.search.brave.com/Zjf5nuQYoWQAvhNEQsSjrK0_RcvIxx0jmbWogsuelYI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZW5k/ZXIuZmluZWFydGFt/ZXJpY2EuY29tL2lt/YWdlcy9pbWFnZXMt/cHJvZmlsZS1mbG93/LzQwMC9pbWFnZXMv/YXJ0d29ya2ltYWdl/cy9tZWRpdW1sYXJn/ZS8yL21vdGhlci10/ZXJlc2Eta2V5c3Rv/bmUtZmVhdHVyZXMu/anBn",
+
+        birthPlace: "Skopje, North Macedonia"
+
+    },
+
+    {
+
+        quote: "When you reach the end of your rope, tie a knot in it and hang on.",
+
+        author: "Franklin D. Roosevelt",
+
+        imgurl: "https://imgs.search.brave.com/amyFSJmqeGz76DjA2jfls5fPIrqkYJdNQii9oFdObc4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTE0/NzAwMjYwL3Bob3Rv/L3ByZXNpZGVudC1m/cmFua2xpbi1kLXJv/b3NldmVsdC10YWxr/cy1vdmVyLXRoZS1y/YWRpby1wcm9taXNl/cy1ob25lc3QtZG9s/bGFyLWluLW5hdGlv/bi13aWRlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1TR05O/eTdGcHR3XzBVQ1dE/Umo0SDdBM0VWclU3/eC1Eem53YmhxeGll/Wm1FPQ",
+
+        birthPlace: "Hyde Park, New York, USA"
+
+    },
+
+    {
+
+        quote: "Always remember that you are absolutely unique. Just like everyone else.",
+
+        author: "Margaret Mead",
+
+        imgurl: "https://imgs.search.brave.com/YGGjnnpcMhLTZtt3SSyu_QEpNjqJXSsvlrkrNe0dm_U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTA3/ODkyMDQ3Mi9waG90/by9wcmVzcy1jb25m/LWZvci1kci1tYXJn/YXJldC1tZWFkLXZp/c2l0aW5nLWFtZXJp/Y2FuLWFudGhyb3Bv/bG9naXN0LXdoby1p/cy1wYXNzaW5nLXRo/cm91Z2guanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPUJLdUtu/TWRLNGU4aGlBQm5C/dm1VZEprTmNMQWo4/YXdUcThzWUExTzhm/YUk9",
+
+        birthPlace: "Philadelphia, Pennsylvania, USA"
+
+    },
+
+    {
+
+        quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+
+        author: "Robert Louis Stevenson",
+
+        imgurl: "https://imgs.search.brave.com/7PdFi4qZsK3a6YqokvrAJ86SIYNYhBNQVgV_eZGoGsQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTk4/Mjc1NjE0MS92ZWN0/b3Ivcm9iZXJ0LWxv/dWlzLXN0ZXZlbnNv/bi1waG90by1wb3J0/cmFpdC1zY290dGlz/aC1hdXRob3ItYW5k/LXBvZXQtMTl0aC1j/ZW50dXJ5LWJyaXRp/c2guanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWtFUUhzQ09z/WHpJaGxWTVd1cnRp/bnNXdDRFNl9UUjhj/ZXBsd21VN1ROLUE9",
+
+        birthPlace: "Edinburgh, Scotland"
+
+    },
+
+    {
+
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+
+        author: "Eleanor Roosevelt",
+
+        imgurl: "https://imgs.search.brave.com/lOHB6nN41CpXNK0LWJSUwxn1SEQmJP9R6O6kEi52qNI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9rYXJz/aC5vcmcvd3AtY29u/dGVudC91cGxvYWRz/LzIwMTYvMTAvWW91/c3VmLUthcnNoLUVs/ZWFub3ItUm9vc2V2/ZWx0LTE5NDQtMTU3/NHgxOTYwLmpwZw",
+
+        birthPlace: "New York City, New York, USA"
+
+    },
+
+    {
+
+        quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+
+        author: "Benjamin Franklin",
+
+        imgurl: "https://imgs.search.brave.com/t-FqYuJ9BIRo1bTqhdTEwIbIa1lKpnvkO4gqua9w-gA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDg2/NjU4MDAwL3Bob3Rv/L2JlbmphbWluLWZy/YW5rbGluLXBvcnRy/YWl0LmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1lUjFHUkRf/U0NJckVMeDZxYzBF/bW9EaUcwaFUtZGhv/MGRTekZQcUVKUDBv/PQ",
+
+        birthPlace: "Boston, Massachusetts, USA"
+
+    },
+
+    {
+
+        quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+
+        author: "Helen Keller",
+
+        imgurl: "https://imgs.search.brave.com/dbXxuwDMNNznNVDWO6rcpjdIAPMkWtiDVEbC-CYCyPk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZW5k/ZXIuZmluZWFydGFt/ZXJpY2EuY29tL2lt/YWdlcy9pbWFnZXMt/cHJvZmlsZS1mbG93/LzQwMC9pbWFnZXMv/YXJ0d29ya2ltYWdl/cy9tZWRpdW1sYXJn/ZS8zLzEtaGVsZW4t/a2VsbGVyLWNhLTE5/MjAtbG9zLWFuZ2Vs/ZXMtdGltZXMuanBn",
+
+        birthPlace: "Tuscumbia, Alabama, USA"
+
+    },
+
+    {
+
+        quote: "It is during our darkest moments that we must focus to see the light.",
+
+        author: "Aristotle",
+
+        imgurl: "https://imgs.search.brave.com/Dzji-BTaZYJr_KTmbcrTIt3X-DYGEyY5Q8QVrc6uWME/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzcyLzAwLzM1/LzM2MF9GXzU3MjAw/MzUzNV9EZDhTQ0V5/NmpORlJWMlByMEJ1/bWZuWmJGQmFJaERY/Ri5qcGc",
+
+        birthPlace: "Stagira, Greece"
+
+    },
+
+    {
+
+        quote: "Whoever is happy will make others happy too.",
+
+        author: "Anne Frank",
+
+        imgurl: "https://imgs.search.brave.com/cB_Q2iAUOY4I5PCIBN5LkOJFhuKVwwfdj1Vg1R0fuYA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9m/L2ZlL0FubmVfRnJh/bmtfbGFjaHRfbmFh/cl9kZV9zY2hvb2xm/b3RvZ3JhYWYuanBn",
+
+        birthPlace: "Frankfurt, Germany"
+
+    },
+
+    {
+
+        quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+
+        author: "Ralph Waldo Emerson",
+
+        imgurl: "https://imgs.search.brave.com/03UryN1CGEsynEfnisBs456zc42nIl-fOHfKELbRjio/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9lbWVy/c29uY2VudHJhbC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjIvMTIvMDRyaWZm/LTItcG9wdXAuanBn",
+
+        birthPlace: "Boston, Massachusetts, USA"
+
+    },
+
+    {
+
+        quote: "You will face many defeats in life, but never let yourself be defeated.",
+
+        author: "Maya Angelou",
+
+        imgurl: "https://imgs.search.brave.com/vsRNfX1X7c3pRsV-AHWCx4eWxVamu_hCYQPGqD2x30g/rs:fit:860:0:0:0/g:ce/aHR0cDovL2ZhbW91/c3BvZXRzYW5kcG9l/bXMuY29tL3BpY3R1/cmVzL21heWFfYW5n/ZWxvdS5qcGc",
+
+        birthPlace: "St. Louis, Missouri, USA"
+
+    },
+
+    {
+
+        quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+
+        author: "Abraham Lincoln",
+
+        imgurl: "https://imgs.search.brave.com/NzxLmZ_yZ6cSgMGGjo8fSziVYtlm5Uw_Y9iC1kxfz9w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMjMvNjIyMjMt/MDA0LTlGNzVGQUVE/L1ByZXNpZGVudC1B/YnJhaGFtLUxpbmNv/bG4tcGFpbnRpbmct/R2VvcmdlLUhlYWx5/LTE4ODctTmF0aW9u/YWwtUG9ydHJhaXQt/R2FsbGVyeS5qcGc",
+
+        birthPlace: "Hodgenville, Kentucky, USA"
+
+    },
+
+    {
+
+        quote: "Never let the fear of striking out keep you from playing the game.",
+
+        author: "Babe Ruth",
+
+        imgurl: "https://imgs.search.brave.com/PFdrHYLyKIVQaDRgj65XVy58ixRPde00Ch5Theds3QE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTE1/NTM1ODI0L3Bob3Rv/L2JhYmUtcnV0aC1j/cm93bmVkLXN1bHRh/bi1vZi1zd2F0LWJ5/LW5ldy15b3JrLWZh/bnMtYWZ0ZXItbGFz/dC1nYW1lLW9mLXRo/ZS13b3JsZC1zZXJp/ZXMtdGhlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz11WmV6/TWhpdi1TcW00YklQ/ekdOa0tHVUFPaWd6/ay1XNjBGSmdzMXlO/Y2lVPQ",
+
+        birthPlace: "Baltimore, Maryland, USA"
+
+    },
+
+    {
+
+        quote: "Life is either a daring adventure or nothing at all.",
+
+        author: "Helen Keller",
+
+        imgurl: "https://imgs.search.brave.com/s_i0ZqXUPDWSS0vGVCJBtHtmLjIiY0TNvWX8x7fY7I4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNjQw/NDc5NTMzL3Bob3Rv/L3BvcnRyYWl0LW9m/LWhlbGVuLWtlbGxl/ci5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9RFJPTkFEc0wt/SktMME1mTUNpLWdE/cndFYzdDMUk5ZmFD/Q0RyY21CZGI1RT0",
+
+        birthPlace: "Tuscumbia, Alabama, USA"
+
+    },
+
+    {
+
+        quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+
+        author: "Thomas A. Edison",
+
+        imgurl: "https://imgs.search.brave.com/1iUgOp8psskjyymeeJV17vgKvGXJymQ7WejEdwSkgAU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZW5k/ZXIuZmluZWFydGFt/ZXJpY2EuY29tL2lt/YWdlcy9pbWFnZXMt/cHJvZmlsZS1mbG93/LzQwMC9pbWFnZXMv/YXJ0d29ya2ltYWdl/cy9tZWRpdW1sYXJn/ZS8yL3Rob21hcy1l/ZGlzb24td2l0aC1o/aXMtcGhvbm9ncmFw/aC1iZXR0bWFubi5q/cGc",
+
+        birthPlace: "Milan, Ohio, USA"
+
+    },
+
+    {
+
+        quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+
+        author: "Brian Tracy",
+
+        imgurl: "https://imgs.search.brave.com/71jjQ6wmpzFmSA5odvUghP41yPy_uVrZraDv6mj4iWY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMxLnBlbmd1aW5y/YW5kb21ob3VzZS5j/b20vYXV0aG9yLzIw/NjUwODM",
+
+        birthPlace: "Charlottetown, Canada"
+
+    },
+
+    {
+
+        quote: "Believe you can and you're halfway there.",
+
+        author: "Theodore Roosevelt",
+
+        imgurl: "https://imgs.search.brave.com/iv09kJfnxBUbSayuH1fMvvy_B8Ty6pilI-RDH8c9MX8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTIy/NjQ4MzU5My9waG90/by91cy1wcmVzaWRl/bnQtdGhlb2RvcmUt/cm9vc2V2ZWx0LW1h/a2VzLWEtc3BlZWNo/LWF0LXNvdXRoLWxh/d3JlbmNlLW1hc3Nh/Y2h1c2V0dHMtY2ly/Y2EtMTkwMi1oaXMu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PXgzZFA4SjJQRldu/UDIyaEYzWkxyaE05/R3ZtMkVUOU1DVmV6/cXdVNmhKLUE9",
+
+        birthPlace: "New York City, New York, USA"
+
+    },
+
+    {
+
+        quote: "Act as if what you do makes a difference. It does.",
+
+        author: "William James",
+
+        imgurl: "https://imgs.search.brave.com/wNyeY9D0C9KKmEFnpIjeWdd7tFuErcp2r7G84JtNuCg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTE1/NTgxOTgwL3Bob3Rv/L3dpbGxpYW0tamFt/ZXMtYW1lcmljYW4t/cHN5Y2hvbG9naXN0/LWFuZC1waGlsb3Nv/cGhlci1waG90by1o/YXJiYXJkLXVuaXZl/cnNpdHkuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPXFSZjR1/SzlERnRMWmJqRE9P/QWc1VDl1RjZ5bkFX/cGdmMkFiQjFHNmk4/aFU9",
+
+        birthPlace: "New York City, New York, USA"
+
+    },
+
+    {
+
+        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+
+        author: "Winston Churchill",
+
+        imgurl: "https://imgs.search.brave.com/JwUuSBEeZRCvBpoQDcsUjGpJtA9fffAMr5FVMSsrLdI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMzM2/NzM4Ny9waG90by93/aW5zdG9uLWNodXJj/aGlsbC1icml0aXNo/LXN0YXRlc21hbi1h/bmQtcHJpbWUtbWlu/aXN0ZXItd2F2aW5n/LWhpcy1oYXQtdG8t/dGhlLWNyb3dkcy1h/dC1kb3duaW5nLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1I/QjZ1OHdMY3VLY0Jq/eElTZGgzaEtKY1Ux/RUREcjEyU2VBSFZh/Qkt2czY4PQ",
+
+        birthPlace: "Woodstock, England"
+
+    },
+
+    {
+
+        quote: "You are never too old to set another goal or to dream a new dream.",
+
+        author: "C.S. Lewis",
+
+        imgurl: "https://imgs.search.brave.com/kTc962klo2DkT_09RTa2J0rDlJwk2rnq7mJZhBzDKF8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vMjQvODI3MjQt/MDUwLUExRjlEMEI5/L0NTLUxld2lzLmpw/Zz93PTQwMCZoPTMw/MCZjPWNyb3A",
+
+        birthPlace: "Belfast, Ireland"
+
+    },
+
+    {
+
+        quote: "At any given moment you have the power to say: this is not how the story is going to end.",
+
+        author: "Christine Mason Miller",
+
+        imgurl: "https://imgs.search.brave.com/n2MgZkmbfYIqCzXUyIRwyOF9M7cpNR8GUPIKha4taUE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3F1YXJlc3Bh/Y2UtY2RuLmNvbS9j/b250ZW50L3YxLzVm/NmE3ZTc2ZTFkMDc4/MGRjOTQwNDVjMy8x/NjEwNDk5NDE4MTg4/LUxPUDBFOVY3M004/M1dWSURZVFJOL2lt/YWdlLWFzc2V0Lmpw/ZWc",
+
+        birthPlace: "Unknown"
+
+    },
+
+    {
+
+        quote: "You don't have to be great to start, but you have to start to be great.",
+
+        author: "Zig Ziglar",
+
+        imgurl: "https://imgs.search.brave.com/Q0E8U1kYIqCCPYbmQcaNpDGgGhLzXlRhDoQYc5AIhvY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/dG9vbHNoZXJvLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MS8wNy96aWctemln/bGFyLXRvb2xzaGVy/by5qcGc",
+
+        birthPlace: "Coffee County, Alabama, USA"
+
+    },
+
+
+    {
+
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+
+        author: "Thomas A. Edison",
+
+        imgurl: "https://imgs.search.brave.com/noerFBQvOz45QFfXGrHRTUD4GEGZ6hqAHqjtGjWb-lU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTg0/OTkyODAvcGhvdG8v/YW1lcmljYW4taW52/ZW50b3ItdGhvbWFz/LWVkaXNvbi1jb25k/dWN0aW5nLWFuLWV4/cGVyaW1lbnQtaW4t/aGlzLWxhYm9yYXRv/cnktY2lyY2EtMTkx/MC5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9M0MtbEhkTTVk/cXp3RXhZZGVjN2N5/RnlvZEd5cVdzd244/azNPUFZ4VGM2bz0",
+
+        birthPlace: "Milan, Ohio, USA"
+
+    },
+
+    {
+
+        quote: "Don't watch the clock; do what it does. Keep going.",
+
+        author: "Sam Levenson",
+
+        imgurl: "https://imgs.search.brave.com/x7EvVGd3oEdBavQPNVj-fM3ejk3hLkK-_NVMt8ZuePg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNDc2/MzgxNzA2L3Bob3Rv/L25ldy15b3JrLXBv/cnRyYWl0LW9mLXNh/bS1sZXZlbnNvbi1o/dW1vcmlzdC13cml0/ZXItdGVsZXZpc2lv/bi1ob3N0LWltYWdl/LWRhdGVkLWphbnVh/cnktMjUtMTk1Ny5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/cjlhelM1N0VQWV8y/NWtObGlyMXc3X2la/NXU2RWl0ZVFWSENM/aUdHSVFNQT0",
+
+        birthPlace: "New York City, New York, USA"
+
+    }
+];
+
+
+var card = document.getElementById("s1");
+
+for (var i = 0; i < Data.length; i++) {
+
+    card.innerHTML += '<div class="col-6"><div class="card" style="margin-left: 5px;"><div class="col-6"><div class="col-12" style="padding: 0px 5px 5px 5px;"><span>Name :-'+Data[i].author+' </span></div><div class="col-12 pad-5"><span>Birth-place :-'+Data[i].birthPlace+'</span></div><div class="col-12" style="padding: 5px 5px 0px 5px;"><span>quote :-'+Data[i].quote+' </span></div></div><div class="col-4"><img src="  '+Data[i].imgurl+'" width="100%"/> </div></div></div>';  
+}
